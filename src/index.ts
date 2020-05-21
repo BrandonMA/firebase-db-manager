498267c716afc3dc7c79d3d482b4a324f8ad0329
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2020-05-20 20:38:37 -0500

    Added persistance by default

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { enableMapSet } from 'immer';
+import * as firebase from 'firebase/app';
+
 enableMapSet();
+
+firebase
+    .firestore()
+    .enablePersistence()
+    .catch(function (err) {
+        if (err.code === 'failed-precondition') {
+            console.error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
+        } else if (err.code === 'unimplemented') {
+            console.error('The current browser does not support all of the features required to enable persistence.');
+        }
+    });
 
 export * from './Models';
 export * from './Types';
