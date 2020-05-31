f21849bf8a3092b7249b98db9ad2c1765a8c63b1
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2020-05-31 18:27:47 -0500

    removed persistance

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,5 @@
 import { enableMapSet } from 'immer';
-import * as firebase from 'firebase/app';
 enableMapSet();
-firebase
-    .firestore()
-    .enablePersistence()["catch"](function (err) {
-    if (err.code === 'failed-precondition') {
-        console.error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
-    }
-    else if (err.code === 'unimplemented') {
-        console.error('The current browser does not support all of the features required to enable persistence.');
-    }
-});
 export * from './Models';
 export * from './Types';
 //# sourceMappingURL=index.js.map
