a660f568d7836938e4b0f1d3141083bf552ff17c
Brandon Maldonado Alonso <bma@fractal.business>
2020-09-29 12:38:07 -0500

    Added small function to upload a file

---
+++ dist/storage/index.d.ts
@@ -0,0 +1,2 @@
+import 'firebase/storage';
+export declare function uploadFile(path: string, filePath: string): Promise<string>;
---
+++ src/storage/index.ts
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
+export async function uploadFile(path: string, filePath: string): Promise<string> {
+    const reference = firebase.storage().ref(path);
+    const response = await fetch(filePath);
+    const blob = await response.blob();
+    await reference.put(blob);
+    return reference.getDownloadURL();
+}
