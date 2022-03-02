450150f8dd8aa69691600f76c8c66862503d8f0c
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2022-03-02 21:43:20 +0100

    Fix typo

--- dist/util/produceFirebaseObject.js
+++ dist/util/produceFirebaseObject.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-exports.produceFirebaseObjectt = void 0;
+exports.produceFirebaseObject = void 0;
 var immer_1 = __importDefault(require("immer"));
 var assignPropertiesToObject_1 = require("./assignPropertiesToObject");
-function produceFirebaseObjectt(newObject, oldObject, assignNullable, callback) {
+function produceFirebaseObject(newObject, oldObject, assignNullable, callback) {
     if (assignNullable === void 0) { assignNullable = false; }
     return immer_1["default"](oldObject, function (draft) {
         callback(draft);
         return assignPropertiesToObject_1.assignPropertiesToObject(draft, newObject, assignNullable);
     });
 }
-exports.produceFirebaseObjectt = produceFirebaseObjectt;
+exports.produceFirebaseObject = produceFirebaseObject;
 //# sourceMappingURL=produceFirebaseObject.js.map
--- src/util/produceFirebaseObject.ts
+++ src/util/produceFirebaseObject.ts
@@ -1,7 +1,7 @@
 import produce, { Draft } from 'immer';
 import { assignPropertiesToObject } from './assignPropertiesToObject';
 
-export function produceFirebaseObjectt<T>(newObject: T, oldObject: T, assignNullable = false, callback: (draft: Draft<T>) => void): T {
+export function produceFirebaseObject<T>(newObject: T, oldObject: T, assignNullable = false, callback: (draft: Draft<T>) => void): T {
     return produce<T>(oldObject, (draft) => {
         callback(draft);
         return assignPropertiesToObject(draft as T, newObject, assignNullable);
